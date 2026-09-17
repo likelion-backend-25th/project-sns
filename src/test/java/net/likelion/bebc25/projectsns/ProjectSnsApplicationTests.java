3f42597715dc5bce0cdf8a5329e0c8fa99f7d5ac
kilyong jeong <uzoolove@gmail.com>
2026-09-17 13:34:33 +0900

    first commit

---
+++ src/test/java/net/likelion/bebc25/projectsns/ProjectSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.likelion.bebc25.projectsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
